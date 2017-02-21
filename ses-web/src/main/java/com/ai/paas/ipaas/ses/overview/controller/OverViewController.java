f918bcf25780d56c2377f18053bbc1f85d015d69
sunhongzhen <sunhz@asiainfo.com>
2017-02-21 19:18:47 +0800

    update

--- ses-web/src/main/java/com/ai/paas/ipaas/ses/overview/controller/OverViewController.java
+++ ses-web/src/main/java/com/ai/paas/ipaas/ses/overview/controller/OverViewController.java
@@ -36,7 +36,7 @@ public class OverViewController {
 			String hostIp;
 			if(hostIpStr != null){
 				String[] hostList =hostIpStr.split(SesConstants.SPLITER_COMMA);
-			    for(int i=0;i<hostList.size();i++){
+			    for(int i=0;i<hostList.length;i++){
 			    	if (hostList[i].contains(ins.getHostIp())){
 			    		hostIp = hostList[i].split(SesConstants.SPLITER_COLON)[1];
 					    break;
